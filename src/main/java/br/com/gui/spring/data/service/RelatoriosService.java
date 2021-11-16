8d2a1bec955a714d3d90b68137d541abf8143b1a
Guilherme <guivicente.gvp@hotmail.com>
2021-11-15 22:33:32 -0300

    Projeto finalizado!

---
+++ src/main/java/br/com/gui/spring/data/service/RelatoriosService.java
@@ -0,0 +1,98 @@
+package br.com.gui.spring.data.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gui.spring.data.orm.Funcionario;
+import br.com.gui.spring.data.orm.FuncionarioProjecao;
+import br.com.gui.spring.data.repository.FuncionarioRepository;
+
+@Service
+public class RelatoriosService {
+
+	private Boolean system = true;
+	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	private final FuncionarioRepository funcionarioRepository;
+
+	public RelatoriosService(FuncionarioRepository funcionarioRepository) {
+		this.funcionarioRepository = funcionarioRepository;
+	}
+
+	public void inicial(Scanner scanner) {
+		while (system) {
+			System.out.println("Qual acao de cargo deseja executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Buscar nome do funcionário");
+			System.out.println("2 - Buscar nome do funcionário, data de contratação e salário");
+			System.out.println("3 - Buscar data de contratação do funcionário");
+			System.out.println("4 - Pesquisa de salário do funcionário");
+			
+			int action = scanner.nextInt();
+
+			switch (action) {
+			case 1:
+				buscaFuncionarioNome(scanner);
+				break;
+			case 2:
+				buscaFuncionarioNomeSalarioMaiorData(scanner);
+				break;
+			case 3:
+				buscaFuncionarioDataContratacao(scanner);
+				break;
+			case 4:
+				pesquisaFuncionarioSalario();
+				break;
+			default:
+				system = false;
+				break;
+			}
+
+		}
+
+	}
+
+	private void buscaFuncionarioNome(Scanner scanner) {
+		System.out.println("Qual o nome que você deseja pesquisar? ");
+		String nome = scanner.next();
+		List<Funcionario> list = funcionarioRepository.findByNome(nome);
+		list.forEach(System.out::println);
+	}
+	
+	private void buscaFuncionarioNomeSalarioMaiorData(Scanner scanner) {
+		System.out.println("Qual o nome que você deseja pesquisar? ");
+		String nome = scanner.next();
+		
+		System.out.println("Qual a data de contratação que você deseja pesquisar? ");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		System.out.println("Qual o salário que você deseja pesquisar? ");
+		Double salario = scanner.nextDouble();
+		
+		List<Funcionario> list = funcionarioRepository.
+				findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
+		list.forEach(System.out::println);
+	}
+	
+	private void buscaFuncionarioDataContratacao(Scanner scanner) {
+		System.out.println("Qual a data de contratação que você deseja pesquisar? ");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
+		list.forEach(System.out::println);
+	}
+	
+	private void pesquisaFuncionarioSalario() {
+		List<FuncionarioProjecao> list = funcionarioRepository.findFuncionarioSalario();
+		list.forEach(f -> System.out.println("Funcionário: " + f.getId() 
+		+ " | Nome:  " + f.getNome() 
+		+ " | Salário: " + f.getSalario()));
+	}
+}
